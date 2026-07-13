416be3e18ae5b062283b413ad0739751a2912f36
Julieta <julieta.insaurralde@gm2dev.com>
2026-07-13 11:14:37 -0300

    Scoreboard (A terminar en funcion a el juego elegido mostrar el puntaje)(Terminar CSS)

--- app/Puntaje/Data/app.js
+++ app/Puntaje/Data/app.js
@@ -1,9 +1,30 @@
-const RankingJugadores = document.getElementById("ranking");
-const url = `https://quiz-api.cesar-kastli.workers.dev/scores`
 
-const scoreboard = getElementById("scoreboard");
+const url = `https://quiz-api.cesar-kastli.workers.dev/games/${gameId}/scores`
+
+const scoreboard = document.getElementById("scoreboard");
+
 async function mostrarScoreboard(){
+    try{
     const respuesta = await fetch(url);
+
+    if(!respuesta.ok){
+        throw new Error("Error para obtener el dato");
+    }
+
     const datos = await respuesta.json()
-    console.log(datos)
+    console.log(datos);
+    scoreboard.innerHTML= "";
+    datos.forEach((jugador, index) => {
+        scoreboard.innerHTML+=`
+        <div class="fila">
+        <span>${index+1}</span>
+        <span>${jugador.name}</span>
+        <span>${jugador.score}</span>
+        </div>`
+    });
+
+    } catch(error){
+        console.log(error);
+    }
 }
+mostrarScoreboard();
