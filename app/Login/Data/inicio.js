a2f3e29e57e22f0fd90b81366fd4511b093a8515
Julieta <julieta.insaurralde@gm2dev.com>
2026-07-14 10:53:38 -0300

    Enlace login al resto de la pagina

--- app/Login/Data/inicio.js
+++ app/Login/Data/inicio.js
@@ -13,6 +13,7 @@
     btnGuardar.addEventListener("click", () => {
         localStorage.setItem("nombre", inputNombre.value);
         inputNombre.disabled = true ;
+        window.location.href ="pantallaSelecciondeJuegos"
     });
 
 
